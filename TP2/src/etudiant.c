901644e956acfa07870419beb1fb805ee9635426
myName <lea.renaud95710@gmail.com>
2025-12-01 10:10:30 +0100

    Update etudiant.c

--- TP2/src/etudiant.c
+++ TP2/src/etudiant.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    // Tableaux pour stocker les données de 5 étudiant.e.s
-    char noms[5][30] = {
-        "Dupont", "Martin", "Durand", "Bernard", "Leroy"
-    };
+    char noms[5][30];
+    char prenoms[5][30];
+    char adresses[5][50];
+    float note_prog[5];
+    float note_sys[5];
 
-    char prenoms[5][30] = {
-        "Alice", "Thomas", "Julie", "Marc", "Sophie"
-    };
+    printf("=== Saisie des informations des 5 etudiants ===\n\n");
 
-    char adresses[5][50] = {
-        "12 rue Victor Hugo",
-        "5 avenue de Paris",
-        "33 boulevard National",
-        "8 chemin Vert",
-        "21 rue de la Gare"
-    };
+    for (int i = 0; i < 5; i++) {
+        printf("Etudiant %d :\n", i + 1);
 
-    // Notes des étudiant.e.s
-    float note_prog[5]  = {15.5, 12.0, 18.0, 10.5, 14.0};
-    float note_sys[5]   = {14.0, 13.5, 17.0, 11.0, 16.0};
+        printf("Nom : ");
+        scanf("%s", noms[i]);
 
-    // Affichage des informations
-    printf("===== LISTE DES ETUDIANTS =====\n\n");
+        printf("Prenom : ");
+        scanf("%s", prenoms[i]);
+
+        printf("Adresse (un seul mot ou remplacez les espaces par _) : ");
+        scanf("%s", adresses[i]);
+
+        printf("Note en Programmation C : ");
+        scanf("%f", &note_prog[i]);
+
+        printf("Note en Systeme d'exploitation : ");
+        scanf("%f", &note_sys[i]);
+
+        printf("\n");
+    }
+
+    printf("\n=== Affichage des informations ===\n\n");
 
     for (int i = 0; i < 5; i++) {
         printf("Etudiant %d :\n", i + 1);
